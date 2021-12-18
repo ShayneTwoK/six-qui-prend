ae0ff5e85bd45915de1e99190d9b5c4ff3239659
Shaïnee <shaynetwok@gmail.com>
2021-12-18 12:31:07 +0100

    update FRONT

---
+++ six-qui-prend/Models/Card.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace six_qui_prend
+{
+    public class Card
+    {
+        public int id { get; set; }
+
+        public int nbrBeefHeads { get; set; }
+
+        public Card(int id, int nbrBeefHeads)
+        {
+            this.id = id;
+            this.nbrBeefHeads = nbrBeefHeads;
+        }
+    }
+}
---
+++ six-qui-prend/Views/ViewCardPlayer.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace six_qui_prend.View
+{
+    /// <summary>
+    /// Logique d'interaction pour ViewCardPlayer.xaml
+    /// </summary>
+    public partial class ViewCardPlayer : UserControl
+    {
+        public ViewCardPlayer()
+        {
+            InitializeComponent();
+        }
+
+        public void displayPlayerCards()
+        {
+            List<Card> PlayerCards =   new List<Card> { new Card(1, 5) };
+
+            foreach (Card card in PlayerCards)
+            {
+                
+            }
+        }
+    }
+}
